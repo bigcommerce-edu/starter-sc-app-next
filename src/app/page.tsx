64b5e169306b4fe3195bed02f8085a491188f610
Chris Nanninga <tarsusc81@gmail.com>
2026-07-02 16:16:20 -0500

    Make home page a pass-through to the gift certificates route
    Removes the standalone home page placeholder in favor of re-exporting
    the /gift-certs page route so both paths render the same content.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,1 @@
-import { Box, Button, Flex, H1, Panel, Text } from "@/components/ui/big-design";
-
-export default function Home() {
-  return (
-    <Flex justifyContent="center" paddingVertical="xxxLarge">
-      <Box marginHorizontal="auto" style={{ maxWidth: "480px", width: "100%" }}>
-        <Panel header="Welcome">
-          <H1>Home page placeholder</H1>
-          <Text marginBottom="large">
-            This is a starting point built with BigCommerce BigDesign components.
-          </Text>
-          <Button>Get started</Button>
-        </Panel>
-      </Box>
-    </Flex>
-  );
-}
+export { default } from "@/app/gift-certs/page";

32ef4b826db5c290de944be0003b153a6713a147
Chris Nanninga <tarsusc81@gmail.com>
2026-07-21 14:10:11 -0500

    Remove redundant Suspense fallback resulting in flickering

--- src/components/gift-certs-manager/customers/detail/customer-detail-page.tsx
+++ src/components/gift-certs-manager/customers/detail/customer-detail-page.tsx
@@ -19,8 +19,8 @@ export async function CustomerDetailPage({
   const storeHashString = Array.isArray(storeHash) ? storeHash[0] : storeHash;
 
   return (
-    <Suspense fallback={<ContentFallback />}>
-      {/* Content fallback because CustomerView must perform data fetching */}
+    <Suspense>
+      {/* Simple Suspense because CustomerView must perform data fetching; higher component already handles fallback */}
       <CustomerView id={idString ?? ""} searchParams={resolvedSearchParams} storeHash={storeHashString} />
     </Suspense>
   );

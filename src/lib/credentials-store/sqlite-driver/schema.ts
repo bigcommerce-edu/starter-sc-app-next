d32766e34ec1e147c8e00a5553517d827948baac
Chris Nanninga <tarsusc81@gmail.com>
2026-08-05 18:51:34 -0500

    TODO: Build the SQLite credentials store driver

--- src/lib/credentials-store/sqlite-driver/schema.ts
+++ src/lib/credentials-store/sqlite-driver/schema.ts
@@ -1,2 +1,4 @@
-// Not implemented yet
+// TODO: Implement the SQLite schema
+//  - CREATE TABLE IF NOT EXISTS for stores, users, store_users (no
+//    store_extensions yet - that's added by the graphql-ext enhancement)
 export const CREATE_CREDENTIALS_STORE_SCHEMA = "";

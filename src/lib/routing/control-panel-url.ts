75ae8e7f2314daef25129e6c0dd090773d5fa257
Chris Nanninga <tarsusc81@gmail.com>
2026-08-05 22:34:48 -0500

    TODO: Link to native BigCommerce control panel pages

---
+++ src/lib/routing/control-panel-url.ts
@@ -0,0 +1,4 @@
+// TODO: Implement getControlPanelUrl - build a real control-panel URL
+//  - normalize path to always start with "/"
+//  - return `https://store-${storeHash}.mybigcommerce.com${normalizedPath}`
+//

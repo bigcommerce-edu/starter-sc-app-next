a566ccc9ef3ea1f6e0214d39ad97ca82a6ca72f5
Chris Nanninga <tarsusc81@gmail.com>
2026-08-05 22:21:07 -0500

    Decorate gift certificates with registered customer accounts

--- src/components/gift-certs-manager/gift-certificates/list/gift-certificate-list-view.tsx
+++ src/components/gift-certs-manager/gift-certificates/list/gift-certificate-list-view.tsx
@@ -1,6 +1,7 @@
 import { cacheLife, cacheTag } from "next/cache";
 import { Panel } from "@bigcommerce/big-design";
 import { GiftCertificateTable } from "@/components/gift-certs-manager/gift-certificates/list/gift-certificate-table";
+import { decorateGiftCertificatesWithRecipientAccounts } from "@/lib/gift-certs-manager/gift-certificates/decorate-with-accounts";
 import { giftCertificateTag, GIFT_CERTIFICATES_LIST_TAG } from "@/lib/gift-certs-manager/gift-certificates/cache-tags";
 import { fetchGiftCertificates } from "@/lib/gift-certs-manager/gift-certificates/gift-certificates-api";
 import { parseGiftCertificatesQuery } from "@/lib/gift-certs-manager/gift-certificates/query";
@@ -30,14 +31,12 @@ export async function GiftCertificateListView({
     cacheTag(giftCertificateTag(item.id));
   }
 
-  // TODO: decorate items with their recipient's registered customer account
-  // via decorateGiftCertificatesWithRecipientAccounts(items, storeHash),
-  // before rendering GiftCertificateTable
+  const decoratedItems = await decorateGiftCertificatesWithRecipientAccounts(items, storeHash);
 
   return (
     <Panel header="Gift Certificates">
       <GiftCertificateTable
-        giftCertificates={items}
+        giftCertificates={decoratedItems}
         hasNextPage={hasNextPage}
         query={query}
         storeHash={storeHash}
